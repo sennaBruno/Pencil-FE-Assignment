82569e99125b0ab018c7d129cf50e353a7c32d48
bruno <themasteremailsl@gmail.com>
2024-11-28 19:17:02 -0300

    feat: configuração inicial do projeto Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { IframePageComponent } from './pages/iframe-page/iframe-page.component';
+import { ChessBoardComponent } from './components/chess-board/chess-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    IframePageComponent,
+    ChessBoardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/iframe-page/iframe-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iframe-page',
+  templateUrl: './iframe-page.component.html',
+  styleUrls: ['./iframe-page.component.css']
+})
+export class IframePageComponent {
+
+}
